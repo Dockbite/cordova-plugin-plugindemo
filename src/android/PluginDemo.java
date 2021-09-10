53eee115e306cb3ede6a1c4b9292afc3fbf42833
Bert Lobbezoo <bert.lobbezoo@dockbite.nl>
2021-09-10 16:30:02 +0200

    Plugin demo

--- src/android/PluginDemo.java
+++ src/android/PluginDemo.java
@@ -1,11 +1,12 @@
-package cordova.plugin.plugindemo;
+package com.dockbite.plugindemo;
+
+import android.widget.Toast;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 /**
  * This class echoes a string called from JavaScript.
@@ -14,19 +15,12 @@ public class PluginDemo extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
-            return true;
+        if(action.equals("nativeToast")){
+            nativeToast();
         }
-        return false;
     }
 
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
+    private void coolMethod() {
+        Toast.makeText(webView.getContext(), "Hello World Cordova Plugin", Toast.LENGTH_SHORT).show();
     }
 }
